6b97c0cd4ca4cf682e29c306afd61d86c6259e99
Kawiya856 <kawiyadissanayaka@gmail.com>
2024-10-05 11:52:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=PBchUTYT#uGilgMoyyUXxxL6M0IlsNlu_tnKeph29B2WRXX7brCI'
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94767514189' : process.env.OWNER_NUMBER,   
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=HIcmhDpI#HlOwtnKac1t7GwazyihPmKvrBPIUj-Iyga95JwDNNxk' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94772484733' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
